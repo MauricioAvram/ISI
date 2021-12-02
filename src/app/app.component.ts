12472c1ed5110c27b66c9fd715431fe47e4d13f5
Mauricio Avram <mauri.avram97@gmail.com>
2021-12-01 22:37:32 -0300

    actualizacion del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ISI';
+
+  constructor(){
+  }
+
+  ngOnInit(): void {
+    this.cargarCombo();
+    
+    
+  }
+
+  provincias = ["Salta","JUJUY","cordoba"]
+
+  
+  cargarCombo(){
+
+    let DOMLocalidad = document.getElementById("localidades");
+    this.provincias.forEach(element => {
+      var option = document.createElement("option");
+
+      option.appendChild(new Option(element.toUpperCase(), element.toLowerCase()))
+      DOMLocalidad?.appendChild(option)
+
+    });
+  }
+  
 }
